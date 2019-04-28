06d363927c3a575d608475c8a4f503112b1a94eb
Brett Mandler <brettmandler@gmail.com>
2019-04-27 20:23:25 -0400

    Remove css reset

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled, { createGlobalStyle } from 'styled-components'
-import reset from 'styled-reset'
 
 import palette from 'theme/palette'
 import Header from 'components/header'
@@ -34,7 +33,6 @@ const GlobalStyle = createGlobalStyle`
   main {
       position: relative;
   }
-  ${reset};
 `
 
 const Layout = ({ children }) => (
