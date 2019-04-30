9350d61d08b43e6bcc955eda8d455209f3bfce5c
Zak Pucci <hello@zakpucci.com>
2019-04-29 23:23:02 -0400

    Init. team carousel component

---
+++ src/components/team-carousel.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const Wrapper = styled.div`
+
+`
+
+const TeamCarousel = () => (
+  <Wrapper>
+    TeamCarousel
+  </Wrapper>
+)
+
+DateInput.propTypes = {
+}
+
+export default TeamCarousel
