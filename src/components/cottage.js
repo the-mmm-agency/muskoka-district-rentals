c9c1866d0c78e0e62268c311f6258da8c5870d19
Zak Pucci <hello@zakpucci.com>
2019-05-07 23:46:13 -0400

    Normalize fonts, spacing, margin, colors on cottages page

--- src/components/cottage.js
+++ src/components/cottage.js
@@ -21,20 +21,33 @@ const Box = styled.div`
 
 const Rating = styled.div`
   margin: ${rhythm(1)} 0;
+  margin-top: 0;
 `
+
+const RatingText = styled.div`
+  display: inline-block;
+  margin-left: 10px;
+  font-size: 14px;
+  vertical-align: text-bottom;
+`
+
 const H2 = styled.h2`
   width: 100%;
+  color: #000;
+  ${scale(0.8)}
 `
 
 const Info = styled.div`
   display: flex;
   flex-wrap: wrap;
   padding: ${rhythm(2)};
+  padding-top: 0;
   width: 33%;
 `
 
 const PrimaryText = styled.span`
   color: ${palette.text.primary};
+  font-weight: 500;
 `
 
 const SecondaryText = styled.span`
@@ -48,6 +61,15 @@ const Image = styled(Img)`
 const Properties = styled.ul`
   color: ${palette.text.secondary};
   list-style: none;
+  margin-left: 0;
+`
+
+const ViewDetails = styled.a`
+  width: 100%;
+  text-align: left;
+  color: #000;
+  font-size: 14px;
+  text-decoration: none;
 `
 
 const UprightText = styled.span`
@@ -56,16 +78,25 @@ const UprightText = styled.span`
 `
 
 const StartFrom = styled.span`
-  ${scale(1 / 2)};
+  ${scale(0.1)};
   color: ${palette.text.secondary};
+  padding-top: 15px;
 `
 const Price = styled.span`
   font-weight: 500;
-  ${scale(3 / 4)}
+  ${scale(0.4)}
+  line-height: 1.875;
+  min-width: 90px;
+  text-align: center;
 `
 
 const Night = styled.span`
-  ${scale(1 / 2)};
+  ${scale(0.1)};
+  padding-top: 15px;
+`
+
+const Description = styled.p`
+  margin-top: 20px;
 `
 
 const StyledNumber = styled(Number)`
@@ -106,13 +137,13 @@ const Cottage = ({
           starDimension="20px"
           name="rating"
         />{' '}
-        {reviewCount} reviews
+        <RatingText>{reviewCount} reviews</RatingText>
       </Rating>
       <H2>{name}</H2>
       <StartFrom>Start from: </StartFrom>
       <Price>{' $' + startFrom}</Price>
       <Night>/Night</Night>
-      <p>{description}</p>
+      <Description>{description}</Description>
       <Properties>
         <li>
           Status:{' '}
@@ -141,6 +172,7 @@ const Cottage = ({
           Pet Friendly: <PrimaryText>{petFriendly ? 'Yes' : 'No'}</PrimaryText>
         </li>
       </Properties>
+      <ViewDetails href="/">View Detail &nbsp;&rsaquo;</ViewDetails>
     </Info>
     <Image fluid={image.childImageSharp.fluid} />
   </Wrapper>
