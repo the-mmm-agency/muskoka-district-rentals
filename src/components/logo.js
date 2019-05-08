dd36e33b52d59f0c9b88ab91d6697c9d4d405bd7
Zak Pucci <hello@zakpucci.com>
2019-05-08 02:17:06 -0400

    Remove Link, use a div

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Img from 'gatsby-image'
 import styled from 'styled-components'
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const Wrapper = styled.div`
   height: auto;
