fd58e79116d3155446471dea932dc0c0c69da525
Zak Pucci <hello@zakpucci.com>
2019-05-08 00:43:31 -0400

    Build out guest services blocks, add placeholder images and text, normalize fonts, margins, padding, containers

--- src/pages/guest-services.js
+++ src/pages/guest-services.js
@@ -5,7 +5,7 @@ import BackgroundImage from 'gatsby-background-image'
 
 import Layout from 'components/layout'
 import SEO from 'components/seo'
-import { rhythm } from 'theme/typography'
+import { scale, rhythm } from 'theme/typography'
 
 const Wrapper = styled.div`
   padding: ${rhythm(1)} 20%;
@@ -17,6 +17,44 @@ const Image = styled(BackgroundImage)`
   &::after {
     filter: brightness(0.8);
   }
+`
+
+const Header = styled.h1`
+  margin-top: 75px;
+  color: #000;
+  ${scale(0.8)}
+  margin-bottom: 10px;
+`
+
+const SecondaryHeader = styled.h6`
+  color: #000;
+  ${scale(0.05)}
+`
+
+const LineItem = styled.div`
+  display: block;
+`
+
+const ItemWrapper = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  &:nth-child(2) {
+    margin-left: 15px;
+  }
+`
+
+const ItemIcon = styled.img`
+  border-radius: 8px;
+`
+
+const ItemHeader = styled.h4`
+  margin-bottom: 10px;
+  color: #000;
+`
+
+const ItemBody = styled.p`
+  margin-bottom: 25px;
+  color: rgba(0, 0, 0, 0.4);
 `
 
 const GuestServices = () => {
@@ -37,28 +75,58 @@ const GuestServices = () => {
       <SEO title="Guest Services" />
       <Image fluid={data.file.childImageSharp.fluid} Tag="section" />
       <Wrapper>
-        <h1>Guest Services</h1>
-        <h6>Reserve your vacation rental with MDR.</h6>
-        <h6>Guest Pre-approval Registration</h6>
-        <p>
-          Veniam consectetur exercitation exercitation laboris fugiat irure in
-          fugiat exercitation ullamco ad Lorem.
-        </p>
-        <h6>Vacation Property Searches</h6>
-        <p>
-          Veniam consectetur exercitation exercitation laboris fugiat irure in
-          fugiat exercitation ullamco ad Lorem.
-        </p>
-        <h6>Guest Concierge</h6>
-        <p>
-          Veniam consectetur exercitation exercitation laboris fugiat irure in
-          fugiat exercitation ullamco ad Lorem.
-        </p>
-        <h6>Travel Insurance</h6>
-        <p>
-          Veniam consectetur exercitation exercitation laboris fugiat irure in
-          fugiat exercitation ullamco ad Lorem.
-        </p>
+        <Header>Guest Services</Header>
+        <SecondaryHeader>
+          Reserve your vacation rental with MDR.
+        </SecondaryHeader>
+        <LineItem>
+          <ItemWrapper>
+            <ItemIcon src="https:/placehold.it/75x75" />
+          </ItemWrapper>
+          <ItemWrapper>
+            <ItemHeader>Guest Pre-approval Registration</ItemHeader>
+            <ItemBody>
+              Veniam consectetur exercitation exercitation laboris fugiat irure
+              in fugiat exercitation ullamco ad Lorem.
+            </ItemBody>
+          </ItemWrapper>
+        </LineItem>
+        <LineItem>
+          <ItemWrapper>
+            <ItemIcon src="https:/placehold.it/75x75" />
+          </ItemWrapper>
+          <ItemWrapper>
+            <ItemHeader>Vacation Property Searches</ItemHeader>
+            <ItemBody>
+              Veniam consectetur exercitation exercitation laboris fugiat irure
+              fugiat exercitation ullamco ad Lorem.
+            </ItemBody>
+          </ItemWrapper>
+        </LineItem>
+        <LineItem>
+          <ItemWrapper>
+            <ItemIcon src="https:/placehold.it/75x75" />
+          </ItemWrapper>
+          <ItemWrapper>
+            <ItemHeader>Guest Concierge</ItemHeader>
+            <ItemBody>
+              Veniam consectetur exercitation exercitation laboris fugiat irure
+              fugiat exercitation ullamco ad Lorem.
+            </ItemBody>
+          </ItemWrapper>
+        </LineItem>
+        <LineItem>
+          <ItemWrapper>
+            <ItemIcon src="https:/placehold.it/75x75" />
+          </ItemWrapper>
+          <ItemWrapper>
+            <ItemHeader>Travel Insurance</ItemHeader>
+            <ItemBody>
+              Veniam consectetur exercitation exercitation laboris fugiat irure
+              fugiat exercitation ullamco ad Lorem.
+            </ItemBody>
+          </ItemWrapper>
+        </LineItem>
       </Wrapper>
     </Layout>
   )
