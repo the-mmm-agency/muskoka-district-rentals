e90e6d27d59a056ecf80ea3d3ebdacde85b5afbb
Brett Mandler <brettmandler@gmail.com>
2019-05-14 01:31:50 -0400

    Package updates

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,12 @@
-import { action } from "@storybook/addon-actions"
-import { addDecorator, configure } from '@storybook/react';
-
+import { action } from '@storybook/addon-actions'
+import { addDecorator, addParameters, configure } from '@storybook/react'
+import { themes } from '@storybook/theming'
 import React from 'react'
 import GlobalStyle from '../src/theme/global.css'
 
-
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context('../stories', true, /\.stories\.js$/)
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach(filename => req(filename))
 }
 
 const withGlobal = cb => (
@@ -24,11 +23,16 @@ global.___loader = {
   hovering: () => {},
 }
 // Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
-global.__PATH_PREFIX__ = ""
+global.__PATH_PREFIX__ = ''
 // This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
 window.___navigate = pathname => {
-  action("NavigateTo:")(pathname)
+  action('NavigateTo:')(pathname)
 }
 
+addParameters({
+  options: {
+    theme: themes.dark,
+  },
+})
 addDecorator(withGlobal)
 configure(loadStories, module)
