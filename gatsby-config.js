6c4e18411cce2242bf263402250cffa0bfe9b043
Brett Mandler <brettmandler@gmail.com>
2019-05-17 22:08:48 -0400

    Remove publicPath

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,6 @@ module.exports = {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
-        publicPath: `admin`,
       },
     },
     {
