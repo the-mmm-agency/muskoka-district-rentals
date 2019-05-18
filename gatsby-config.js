0558e9bf3221d7f6c9a1ec46fb27bd78f2e5ab96
Brett Mandler <brettmandler@gmail.com>
2019-05-17 22:01:29 -0400

    Fix netlify build

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,7 @@ module.exports = {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
+        publicPath: `admin`,
       },
     },
     {
