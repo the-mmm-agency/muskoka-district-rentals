3d5aa07e76e7d4830c673824305bc7e23118688a
Brett Mandler <brettmandler@gmail.com>
2019-07-04 18:50:31 -0400

    Use no text icon on mobile

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -4,6 +4,7 @@ import styled from '@xstyled/emotion'
 import { graphql, useStaticQuery } from 'gatsby'
 
 import { up, down } from 'theme/media'
+import Hidden from 'components/hidden'
 import Link from 'components/link'
 
 const Wrapper = styled(Link)`
@@ -29,17 +30,35 @@ const Logo = props => {
           }
         }
       }
+      icon: file(relativePath: { eq: "muskoka-icon.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300, maxHeight: 300) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
     }
   `)
   return (
     <Wrapper to="/" {...props}>
-      <Img
-        alt="Muskoka District Rentals"
-        imgStyle={{
-          objectFit: 'contain',
-        }}
-        fluid={data.logo.childImageSharp.fluid}
-      />
+      <Hidden down="md">
+        <Img
+          alt="Muskoka District Rentals"
+          imgStyle={{
+            objectFit: 'contain',
+          }}
+          fluid={data.logo.childImageSharp.fluid}
+        />
+      </Hidden>
+      <Hidden up="md">
+        <Img
+          alt="Muskoka District Rentals"
+          imgStyle={{
+            objectFit: 'contain',
+          }}
+          fluid={data.icon.childImageSharp.fluid}
+        />
+      </Hidden>
     </Wrapper>
   )
 }
