ffdf5c812a18ca5de85154f5ab5597a0884a0c22
Brett Mandler <brettmandler@gmail.com>
2019-08-01 19:30:36 -0400

    Attempt to mitigate issues with build failures

--- normalizeWordpress.js
+++ normalizeWordpress.js
@@ -17,51 +17,55 @@ const normalizeWordpress = ({ entities }) => {
   return entities.map(e => {
     if (e.__type === 'wordpress__wp_mphb_room_type') {
       try {
-        e.bookings___NODE = bookings
-          .filter(booking => booking.mphb_reserved_room_id === e.wordpress_id)
-          .map(booking => booking.id)
-      } catch (error) {
-        console.log(bookings)
-      }
-      e.categories___NODE = e.mphb_room_type_category.map(
-        c => categories.find(gObj => c === gObj.wordpress_id).id
-      )
-      e.amenities___NODE = e.mphb_room_type_facility.map(
-        c => amenities.find(gObj => c === gObj.wordpress_id).id
-      )
-      e.suitability___NODE = e.mphb_ra_suitability.map(
-        c => suitability.find(gObj => c === gObj.wordpress_id).id
-      )
-      e.services___NODE = e.services.map(
-        c => services.find(gObj => c === gObj.wordpress_id.toString()).id
-      )
-      e.images___NODE = e.gallery
-        .split(',')
-        .map(c => media.find(gObj => c === gObj.wordpress_id.toString()).id)
-      e.lowestRate = rates.find(
-        gObj => e.wordpress_id.toString() === gObj.roomType
-      ).seasonPrices[0].price.prices[0]
-      e.rates = rates
-        .filter(gObj => e.wordpress_id.toString() === gObj.roomType)
-        .map(rate => ({
-          ...rate,
-          seasonPrices: rate.seasonPrices.map(seasonPrices => ({
-            ...seasonPrices,
-            season: seasons
-              .filter(
-                gObj => gObj.wordpress_id.toString() === seasonPrices.season
-              )
-              .map(season => ({
-                ...season,
-                days: season.days.map(day => parseInt(day, 10)),
-              })),
-          })),
-        }))
+        try {
+          e.bookings___NODE = bookings
+            .filter(booking => booking.mphb_reserved_room_id === e.wordpress_id)
+            .map(booking => booking.id)
+        } catch (error) {
+          console.error('Fetching bookings failed', error)
+        }
+        e.categories___NODE = e.mphb_room_type_category.map(
+          c => categories.find(gObj => c === gObj.wordpress_id).id
+        )
+        e.amenities___NODE = e.mphb_room_type_facility.map(
+          c => amenities.find(gObj => c === gObj.wordpress_id).id
+        )
+        e.suitability___NODE = e.mphb_ra_suitability.map(
+          c => suitability.find(gObj => c === gObj.wordpress_id).id
+        )
+        e.services___NODE = e.services.map(
+          c => services.find(gObj => c === gObj.wordpress_id.toString()).id
+        )
+        e.images___NODE = e.gallery
+          .split(',')
+          .map(c => media.find(gObj => c === gObj.wordpress_id.toString()).id)
+        e.lowestRate = rates.find(
+          gObj => e.wordpress_id.toString() === gObj.roomType
+        ).seasonPrices[0].price.prices[0]
+        e.rates = rates
+          .filter(gObj => e.wordpress_id.toString() === gObj.roomType)
+          .map(rate => ({
+            ...rate,
+            seasonPrices: rate.seasonPrices.map(seasonPrices => ({
+              ...seasonPrices,
+              season: seasons
+                .filter(
+                  gObj => gObj.wordpress_id.toString() === seasonPrices.season
+                )
+                .map(season => ({
+                  ...season,
+                  days: season.days.map(day => parseInt(day, 10)),
+                })),
+            })),
+          }))
 
-      delete e.mphb_room_type_facility
-      delete e.mphb_room_type_category
-      delete e.mphb_ra_suitability
-      delete e.gallery
+        delete e.mphb_room_type_facility
+        delete e.mphb_room_type_category
+        delete e.mphb_ra_suitability
+        delete e.gallery
+      } catch (error) {
+        console.error('Error adding the property', error)
+      }
     }
     return e
   })
