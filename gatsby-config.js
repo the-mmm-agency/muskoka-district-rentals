471202c99f4863f32ec87f52f6ba91c01ea81804
Brett Mandler <brettmandler@gmail.com>
2019-09-23 07:24:01 -0400

    Added chatbot

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,6 +115,12 @@ module.exports = {
     },
 
     // Deployment
+    {
+      resolve: 'gatsby-plugin-drift',
+      options: {
+        appId: '46w3n56cw9as',
+      },
+    },
     'gatsby-plugin-preload-link-crossorigin',
     {
       resolve: 'gatsby-plugin-netlify',
