9d0361f06742db6172bdd71b449607d94ecdb75e
brettm12345 <brettmandler@gmail.com>
2020-01-08 23:07:58 -0500

    Fix button

--- src/components/layout/header/header.desktop.tsx
+++ src/components/layout/header/header.desktop.tsx
@@ -1,12 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC } from 'react'
 
-import { Nav, StyledHeader } from './header.desktop.css';
-import HeaderLink, { StyledA } from './header.link';
-import Logo from './header.logo';
+import { Nav, StyledHeader } from './header.desktop.css'
+import HeaderLink, { StyledA } from './header.link'
+import Logo from './header.logo'
 
-import Hidden from 'components/hidden';
-import ProductionLink from 'components/productionLink/index';
-import Button from 'elements/button';
+import Hidden from 'components/hidden'
+import ProductionLink from 'components/productionLink/index'
+import Button from 'elements/button'
 
 interface HeaderProps {
   links: string[];
@@ -34,11 +34,16 @@ const Header: FC<HeaderProps> = ({ links }) => (
       ))}
     </Nav>
     <Hidden down="xl">
-      <ProductionLink to="/properties">
-        <Button variant="serif" size="wide" role="link" m={2}>
-          Book Now
-        </Button>
-      </ProductionLink>
+      <Button
+        as={ProductionLink}
+        to="/properties"
+        variant="serif"
+        size="wide"
+        role="link"
+        m={2}
+      >
+        Book Now
+      </Button>
     </Hidden>
   </StyledHeader>
 );
