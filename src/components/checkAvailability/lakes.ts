bc3ad02b89c06098a8f69648432360fb797a3d32
brettm12345 <brettmandler@gmail.com>
2020-02-28 18:47:41 -0500

    Update lake order

--- src/components/checkAvailability/lakes.ts
+++ src/components/checkAvailability/lakes.ts
@@ -1,26 +1,23 @@
 import * as A from 'fp-ts/lib/Array'
 
 export const LakeNames = [
-  'Georgian Bay',
   'Lake Joseph',
+  'Lake Rosseau',
   'Lake Muskoka',
   'Lake of Bays',
-  'Lake Rosseau',
   'Lake Simcoe',
-  'Toronto',
   'Haliburton',
-  'Other Lakes',
+  'Toronto',
 ];
 
 export const LakeIds = [
-  'ad3cd275100718ff',
   '10d0e6c133279360',
   'bf0bee3a74d7f870',
   'afc29f03f16ed586',
   'af3052a85311ccfe',
   'affc17d6f20a4755',
-  '1466847ff885aa34',
   '1bd9a3b260f967ef',
+  '1466847ff885aa34',
 ];
 
 export const LakePairs = A.zip(LakeNames, LakeIds);
