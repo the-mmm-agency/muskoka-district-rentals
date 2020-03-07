28cc2f6c75653f14ab9196db3e9702a37fc76792
brettm12345 <brettmandler@gmail.com>
2020-03-07 16:35:22 -0500

    Added gatsby favicons plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,33 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/images/mdr.svg',
+
+        // WebApp Manifest Configuration
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+    {
       resolve: 'gatsby-plugin-mdx',
       options: {
         gatsbyRemarkPlugins: [
