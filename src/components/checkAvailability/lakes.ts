c087428e8af8182ff1bf00105097454983c6ccdb
brettm12345 <brettmandler@gmail.com>
2020-03-10 10:51:54 -0400

    Updated lakes and logo

--- src/components/checkAvailability/lakes.ts
+++ src/components/checkAvailability/lakes.ts
@@ -4,7 +4,7 @@ export const LakeNames = {
   'Lake Rosseau': 'd2f8ff8a2cc1e9a1',
   'Lake Muskoka': '9c78ea096e6aa845',
   'Lake of Bays': '95cf8e560435499f',
-  'Other Lakes': '',
+  'Other Lakes': '23d8fa6a1fc64238',
   Haliburton: '1bd9a3b260f967ef',
   'Lake Simcoe': 'cf763491e6c296e7',
   Toronto: '1466847ff885aa34',
