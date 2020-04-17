56138195640a1d0f2f40f9d6d7f4dd5428ace3f9
brettm12345 <brettmandler@gmail.com>
2020-04-17 11:54:14 -0400

    Update concierge services

--- src/components/conciergeServices/conciergeServices.tsx
+++ src/components/conciergeServices/conciergeServices.tsx
@@ -1,15 +1,18 @@
-import styled from '@xstyled/emotion'
-import { graphql, useStaticQuery } from 'gatsby'
-import React, { useState } from 'react'
+import styled, { css } from '@xstyled/emotion';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
+import React, { useState } from 'react';
+import Fade from 'react-reveal/Fade';
 
-import ConciergeService from './conciergeService'
+import ConciergeService from './conciergeService';
 
-import Box from 'elements/box'
-import Flex from 'elements/flex'
-import Heading from 'elements/heading'
-import Text from 'elements/text'
-import scrollbars from 'styles/scrollbars.css'
-import { up } from 'theme/media'
+import Hidden from 'components/hidden';
+import Box from 'elements/box';
+import Flex from 'elements/flex';
+import Heading from 'elements/heading';
+import Text from 'elements/text';
+import scrollbars from 'styles/scrollbars.css';
+import { up } from 'theme/media';
 
 const List = styled.ul`
   ${up('md')} {
@@ -47,25 +50,39 @@ const ConciergeServices = ({ rentalPage = false }) => {
   return (
     <Flex
       as="section"
-      minHeight={{ xs: 900 }}
-      maxHeight={{ xs: 900 }}
+      minHeight={{ xs: 900, lg: 800 }}
+      maxHeight={{ xs: 900, lg: 800 }}
       flexGrow={0}
     >
-      <Box col={{ xs: 1 }} py={{ xs: 4 }} pl={{ xs: 4 }} pr={{ xs: 4 }}>
-        <Box textAlign={{ xs: 'center', sm: 'left' }}>
-          <Text letterSpacing="headers" pl={2} textTransform="uppercase">
-            Experience Muskoka
-          </Text>
-          <Heading mt={2} mb={5}>
-            Concierge Services
-          </Heading>
-        </Box>
+      <Box
+        col={{ xs: 1, lg: 0.5 }}
+        py={{ xs: 4, md: 5 }}
+        pl={{ xs: 4, md: 5 }}
+        pr={{ xs: 4, lg: 0 }}
+      >
+        {!rentalPage && (
+          <Box textAlign={{ xs: 'center', sm: 'left' }}>
+            <Text letterSpacing="headers" pl={2} textTransform="uppercase">
+              Experience Muskoka
+            </Text>
+            <Heading mt={2} mb={5}>
+              Concierge Services
+            </Heading>
+          </Box>
+        )}
         <List>
           {services.map(({ name, ...rest }) => (
             <ConciergeService key={name} setSelected={setSelected} {...rest} />
           ))}
         </List>
       </Box>
+      <Hidden down="lg">
+        <Box col={0.5} minHeight="100%">
+          <Fade spy={selected} mountOnEnter>
+            <Img fluid={selected.childImageSharp.fluid} />
+          </Fade>
+        </Box>
+      </Hidden>
     </Flex>
   );
 };
